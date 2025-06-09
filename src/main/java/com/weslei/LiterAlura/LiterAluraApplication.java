7c8813a1bac8878fae371794c24d8a5122632958
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2025-06-09 16:04:36 -0400

    Initial commit

---
+++ src/main/java/com/weslei/LiterAlura/LiterAluraApplication.java
@@ -0,0 +1,20 @@
+package com.weslei.LiterAlura;
+
+import com.weslei.LiterAlura.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
+}
---
+++ src/main/java/com/weslei/LiterAlura/principal/Principal.java
@@ -0,0 +1,12 @@
+package com.weslei.LiterAlura.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+
+    public void exibeMenu() {
+    }
+
+
+}
